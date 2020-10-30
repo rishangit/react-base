cc6bb5b4bd28389ecf22c9aaefb2d21a33a38bfe
rishan karunarathna <r.karunarathna@sana-commerce.com>
2020-10-31 03:13:04 +0530

    loging and logout bug fixed

--- src/system/common/modules/element/index.js
+++ src/system/common/modules/element/index.js
@@ -1,4 +1,4 @@
 export { default as Button } from './button';
-export { default as TextBox } from './textBox';
+export { default as TextBox } from './textbox';
 export { default as Label } from './label';
 export { default as Message } from './message';
